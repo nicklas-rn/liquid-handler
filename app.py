3ce18b4d9c600560be9f327e01b07dfef77d884b
nicklas-rn <nicklas.rondot@gmail.com>
2023-12-14 17:20:04 -0800

    fix: configured microstepping for syringe stepper and adapted values accordingly + fixed mm per µl for syringe

--- app.py
+++ app.py
@@ -41,6 +41,7 @@ def control_panel():
 
     try:
         selected_port = lh.port.replace('/dev/', '')
+        send_gcode(f'$113=1000\r\n')
     except:
         selected_port = ''
 
@@ -90,10 +91,10 @@ def select_port():
     #Step idle delay (ms)
     send_gcode('$1=10\r\n')
     #Steps per mm
-    send_gcode('$100=4.9736\r\n')
+    send_gcode('$100=79.578\r\n')
     send_gcode('$101=79.578\r\n')
-    send_gcode('$102=100\r\n')
-    send_gcode('$103=40\r\n')
+    send_gcode('$102=1600\r\n')
+    send_gcode('$103=640\r\n')
     #Max rate (mm/min)
     send_gcode('$110=2000\r\n')
     send_gcode('$111=2000\r\n')
@@ -130,23 +131,33 @@ def set_home():
 
 def syringe_wellplate_run(syringe_volume, well_data_list):
     if syringe_volume == 1:
-        syringe_mm_per_µl = 0.01393
+        syringe_mm_per_µl = 0.055726
     elif syringe_volume == 3:
-        syringe_mm_per_μl = 0.00424
+        syringe_mm_per_μl = 0.016978
+    send_gcode(f"G0 X0 Y0 Z10\r\n")
+    time.sleep(5)
     for i, row in enumerate(well_data_list):
         y = - i * 9
+        old_x = 0
         for j, well in enumerate(row):
             if well['volume'] != '':
                 x = j * 9
                 volume = float(well['volume'])
                 flowrate = float(well['flowrate'])
+
                 stepper_rate = syringe_mm_per_μl * 1000 * flowrate
                 stepper_distance = volume * syringe_mm_per_μl
                 send_gcode(f'$113={stepper_rate}\r\n')
                 send_gcode(f"G0 X{x} Y{y} Z10\r\n")
                 send_gcode(f"G0 X{x} Y{y} Z0\r\n")
-                send_gcode(f"G0 X{x} Y{y} Z0 A-{stepper_distance}\r\n")
+                send_gcode(f"G91\r\nG0 A-1\r\nG90\r\n")
+                send_gcode(f"G91\r\nG0 A-{stepper_distance}\r\nG90\r\n")
+                send_gcode(f"G91\r\nG0 A+1\r\nG90\r\n")
                 send_gcode(f"G0 X{x} Y{y} Z10\r\n")
+                sleep_time = ((stepper_distance + 2) / stepper_rate + 20/200 + (x-old_x)/1000) * 60
+                print(sleep_time)
+                time.sleep(sleep_time)
+                old_x = x
 
 
 @app.route('/syringe_wellplate_start_run', methods=['POST'])
